3fa82cd1b03bac358ef89de56108ea04926bca69
mo <mo.noorani@icloud.com>
2016-10-17 15:31:48 +0100

    test

---
+++ Week1/d1_q4.groovy
@@ -0,0 +1,26 @@
+/*
+Day 1 - Q4 Prime numbers
+Write a program that asks a number from the user, then says whether the number is prime or not. Remember that
+a prime number is a number that is divisible only by 1 and itself. You can use the modulo operator (if a % b is
+zero, then a is divisible by b).
+*/
+
+
+
+String str = ""
+boolean prime = true
+
+if (str != "exit") {
+	println "Enter number or type 'exit' to quite: "
+	str = System.console()readLine()
+	int num = Integer.parseInt(str)
+	for (i = 2; i < num; i++) {
+		if (num % i == 0) {
+			prime = false
+			println "number is NOT prime"
+			break
+		} else {
+			println "number IS prime"
+		}
+	}
+}
